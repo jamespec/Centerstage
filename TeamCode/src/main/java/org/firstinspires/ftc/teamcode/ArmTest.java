38b3394f8a59977790219449590eec5dde62f6f8
jamespec <38446091+jamespec@users.noreply.github.com>
2023-10-14 20:35:38 -0400

    Concepts for AprilTags, Rev2mDistance and Motor testing.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmTest.java
@@ -1,28 +1,28 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-// Make it differenc
-
 @TeleOp
-public class ArmTest extends OpMode {
-    int power  = 0;
-    private DcMotor arm = null;
-
+public class ArmTest extends LinearOpMode {
     @Override
-    public void init() {
+    public void runOpMode() {
         telemetry.addData("Status", "Initializing");
 
-        arm = hardwareMap.get(DcMotor.class, "arm");
+        DcMotor arm = hardwareMap.get(DcMotor.class, "arm");
         arm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        arm.setPower(power);
-    }
 
-    @Override
-    public void loop() {
+        telemetry.addData("Status", "Press start...");
+        waitForStart();
+        while(opModeIsActive()) {
+            // Run wheels in tank mode (note: The joystick goes negative when pushed forward, so negate it)
+            double power = -gamepad1.left_stick_y / 5;
 
+            telemetry.addData("Power: ", power);
+            telemetry.update();
+            arm.setPower( power );
+        }
     }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorREV2mDistance.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorREV2mDistance.java
@@ -33,9 +33,9 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
@@ -51,15 +51,22 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
  * See the sensor's product page: https://www.revrobotics.com/rev-31-1505/
  */
 @TeleOp(name = "Sensor: REV2mDistance", group = "Sensor")
-@Disabled
+// senso@Disabled
 public class SensorREV2mDistance extends LinearOpMode {
 
     private DistanceSensor sensorDistance;
+    private DigitalChannel green;
+    private DigitalChannel red;
+
 
     @Override
     public void runOpMode() {
         // you can use this as a regular DistanceSensor.
         sensorDistance = hardwareMap.get(DistanceSensor.class, "sensor_distance");
+        green = hardwareMap.get(DigitalChannel.class, "green");
+        green.setMode(DigitalChannel.Mode.OUTPUT);
+        red = hardwareMap.get(DigitalChannel.class, "red");
+        red.setMode(DigitalChannel.Mode.OUTPUT);
 
         // you can also cast this to a Rev2mDistanceSensor if you want to use added
         // methods associated with the Rev2mDistanceSensor class.
@@ -68,20 +75,37 @@ public class SensorREV2mDistance extends LinearOpMode {
         telemetry.addData(">>", "Press start to continue");
         telemetry.update();
 
+        boolean redState = false;
+        boolean greenState = false;
+
         waitForStart();
         while(opModeIsActive()) {
+            double distanceInches = sensorDistance.getDistance(DistanceUnit.INCH);
             // generic DistanceSensor methods.
             telemetry.addData("deviceName", sensorDistance.getDeviceName() );
             telemetry.addData("range", String.format("%.01f mm", sensorDistance.getDistance(DistanceUnit.MM)));
             telemetry.addData("range", String.format("%.01f cm", sensorDistance.getDistance(DistanceUnit.CM)));
             telemetry.addData("range", String.format("%.01f m", sensorDistance.getDistance(DistanceUnit.METER)));
-            telemetry.addData("range", String.format("%.01f in", sensorDistance.getDistance(DistanceUnit.INCH)));
+            telemetry.addData("range", String.format("%.01f in", distanceInches));
 
             // Rev2mDistanceSensor specific methods.
             telemetry.addData("ID", String.format("%x", sensorTimeOfFlight.getModelID()));
             telemetry.addData("did time out", Boolean.toString(sensorTimeOfFlight.didTimeoutOccur()));
 
             telemetry.update();
+
+            if( distanceInches > 10 ) {
+                green.setState(false);
+                red.setState(true);
+            }
+            else if( distanceInches > 5 ) {
+                green.setState(false);
+                red.setState(false);
+            }
+            else {
+                green.setState(true);
+                red.setState(false);
+            }
         }
     }
 
